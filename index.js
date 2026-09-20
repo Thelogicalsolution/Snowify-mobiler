0d65abc3b0461dec8109cffa0fdd758f83b3a4eb
Thelogicalsolution <lforlogic2@proton.me>
2026-09-20 14:14:42 +0300

    replace patch-package with custom postinstall script for track-player fix

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-/**
- * @format
- */
+PS F:\Projects\SnowifyMobile> npm install react-native-track-player
 
-import { AppRegistry } from 'react-native';
-import TrackPlayer from 'react-native-track-player';
-import App from './App';
-import { name as appName } from './app.json';
-import { PlaybackService } from './PlaybackService';
+added 1 package, and audited 885 packages in 8s
 
-AppRegistry.registerComponent(appName, () => App);
-TrackPlayer.registerPlaybackService(() => PlaybackService);
+184 packages are looking for funding
+  run `npm fund` for details
+
+4 high severity vulnerabilities
+
+To address all issues, run:
+  npm audit fix
+
+Run `npm audit` for details.
+PS F:\Projects\SnowifyMobile>
